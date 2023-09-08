40e1ed9a40bfdaf3e52302185abc9f6b1b0dbc51
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2023-09-08 12:43:36 +0300

    Main menu screen

--- core/src/com/game/asteroids/Asteroids.java
+++ core/src/com/game/asteroids/Asteroids.java
@@ -3,24 +3,41 @@ package com.game.asteroids;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Asteroids extends Game {
-	SpriteBatch batch;
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private MainMenuScreen mainMenuScreen;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		font = new BitmapFont();
+
+		mainMenuScreen = new MainMenuScreen(this);
+		setScreen(mainMenuScreen);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
+		font.dispose();
+		mainMenuScreen.dispose();
+	}
+
+	public BitmapFont getBitmapFont() {
+		return font;
+	}
+
+	public SpriteBatch getSpriteBatch() {
+		return batch;
 	}
 }
